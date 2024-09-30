5eed8d4c60dd142a17cf94caf2cf9cb1c29b9848
Delmiro-Ribeiro <delmiroribeiro.alpha@gmail.com>
2024-09-30 18:42:39 -0300

    primeiro commit

---
+++ planeta-limpo/src/main/java/com/del_dev/planeta_limpo/PlanetaLimpoApplication.java
@@ -0,0 +1,13 @@
+package com.del_dev.planeta_limpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetaLimpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetaLimpoApplication.class, args);
+	}
+
+}
---
+++ planeta-limpo/src/test/java/com/del_dev/planeta_limpo/PlanetaLimpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.del_dev.planeta_limpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetaLimpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
